57c3dc4a1f9f56fa8e7a1abed298c79a184545ba
jnorgini <jnorgini@gmail.com>
2023-06-11 16:17:25 -0300

    new queries

--- backend/src/main/java/com/norgini/dslist/services/GameService.java
+++ backend/src/main/java/com/norgini/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.norgini.dslist.dto.GameDTO;
 import com.norgini.dslist.dto.GameMinDTO;
 import com.norgini.dslist.entities.Game;
+import com.norgini.dslist.projections.GameMinProjection;
 import com.norgini.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,11 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
