60567d27b8557b37258db2a02558e932ff42e175
jnorgini <jnorgini@gmail.com>
2023-06-11 15:14:52 -0300

    entities, controller, dto, repository, services and sql

---
+++ backend/src/main/java/com/norgini/dslist/services/GameService.java
@@ -0,0 +1,32 @@
+package com.norgini.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.norgini.dslist.dto.GameDTO;
+import com.norgini.dslist.dto.GameMinDTO;
+import com.norgini.dslist.entities.Game;
+import com.norgini.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+}
